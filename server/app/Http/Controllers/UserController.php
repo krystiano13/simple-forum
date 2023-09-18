ad216698f2c40208d223c4acc43848fed9c80aec
krystiano13 <crystianotv@gmail.com>
2023-09-18 20:28:42 +0200

    registration on backend

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    
+    public function register(Request $request) {
+        $fields = $request -> all();
+
+        $validator = Validator::make($fields,[
+            'name' => ['required', 'unique:users'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'confirmed', 'min:8']
+        ]);
+
+        if($validator -> fails()) {
+            return response() -> json(
+                [
+                    "status" => false,
+                    "message" => "Validation Error",
+                    "errors" => $validator -> errors()
+                ], 401
+            );
+        }
+
+        else {
+            $fields['password'] = bcrypt($fields['password']);
+            User::create($fields);
+
+            return response() -> json([
+                'status' => true,
+                'message' => "Account Created"
+            ],200);
+        }
+    } 
 }

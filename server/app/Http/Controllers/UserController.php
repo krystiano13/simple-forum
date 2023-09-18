4c74c7e598fc36e4aeb908f86e4c8e04e9c1e693
krystiano13 <crystianotv@gmail.com>
2023-09-18 20:35:44 +0200

    create loggin in on backend'

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -8,6 +8,44 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
+    public function login(Request $request) {
+        $fields = $request -> all();
+
+        $validator = Validator::make($fields,[
+            'name' => 'required',
+            'password' => 'required'
+        ]);
+
+        if($validator -> fails()) {
+            return response() -> json([
+                'status' => 'false',
+                'message' => 'Validation error',
+                'errors' => $validator -> errors()
+            ],401);
+        }
+
+        else if(!auth() -> attempt([
+            'name' => $fields['name'],
+            'password' => $fields['password']
+        ])) {
+            return response() -> json([
+                'status' => false,
+                'message' => 'Wrong username or password'
+            ],401);
+        }
+
+        $token = auth() -> user() -> createToken(
+            auth() -> user() -> name,
+            expiresAt: now() -> addHour()
+        );
+
+        return response() -> json([
+            'status' => true,
+            'message' => 'Logged In successfully',
+            'token' => $token -> accessToken
+        ],200);
+    }
+
     public function register(Request $request) {
         $fields = $request -> all();
 

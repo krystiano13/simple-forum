b1e9db234af549d0394fad4f437dcac3bd0eb99d
krystiano13 <crystianotv@gmail.com>
2023-09-21 17:09:25 +0200

    add switching buttons

--- client/src/components/best/Best.tsx
+++ client/src/components/best/Best.tsx
@@ -1,20 +1,60 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import BestStyles from "./Best.css?inline";
 
 export const Best = component$(() => {
   useStylesScoped$(BestStyles);
+  const mode = useSignal(true);
 
   return (
     <div class="best flex jc-center flex-col pl-6">
-      <h2 class="f-xl font-head f-600 color text-left">Best Users:</h2>
+      <h2 class="f-xl font-head f-600 color text-left mt-6">Best Users:</h2>
       <div class="panel mt-2 bg-primary p-3">
         <div class="buttons flex">
-          <button id="b1" class="font-head f-600 p-1 bg-secondary color">Comments</button>
-          <button id="b2" class="font-head f-600 p-1 bg-accent">Posts</button>
+          <button
+            onClick$={() => {
+              mode.value = true;
+            }}
+            class={
+              mode.value
+                ? "b1 font-head f-600 p-1 bg-secondary color"
+                : "b2 font-head f-600 p-1 bg-accent"
+            }
+          >
+            Comments
+          </button>
+          <button
+            onClick$={() => {
+              mode.value = false;
+            }}
+            class={
+              !mode.value
+                ? "b1 font-head f-600 p-1 bg-secondary color"
+                : "b2 font-head f-600 p-1 bg-accent"
+            }
+          >
+            Posts
+          </button>
         </div>
         <div class="users mt-2">
           <div class="user">
-            <p>Admin (2137)</p>
+            <div class="block bg-secondary color font-other f-400 p-2 m-1 br-1">
+              Admin (2137)
+            </div>
+            <div class="block bg-secondary color font-other f-400 p-2 m-1 br-1">
+              Admin (2137)
+            </div>
+            <div class="block bg-secondary color font-other f-400 p-2 m-1 br-1">
+              Admin (2137)
+            </div>
+            <div class="block bg-secondary color font-other f-400 p-2 m-1 br-1">
+              Admin (2137)
+            </div>
+            <div class="block bg-secondary color font-other f-400 p-2 m-1 br-1">
+              Admin (2137)
+            </div>
+            <div class="block bg-secondary color font-other f-400 p-2 m-1 br-1">
+              Admin (2137)
+            </div>
           </div>
         </div>
       </div>

30e0415ba36c8ceb279e634998037c0a56110d92
krystiano13 <crystianotv@gmail.com>
2023-09-22 10:57:40 +0200

    implement counting posts on server side

--- server/app/Http/Controllers/PostsCountController.php
+++ server/app/Http/Controllers/PostsCountController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PostCount;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class PostsCountController extends Controller
 {
-    
+    public function getCount(Request $request) {
+        return response() -> json(
+            PostCount::orderBy('count', 'DESC') -> take(6) -> get()
+        );
+    }
+
+    public function increment(Request $request) {
+        $fields = $request -> all();
+
+        $validator = Validator::make($fields,[
+            'username' => ['required']
+        ]);
+
+        if(!$validator -> fails()) {
+            $user = User::where('name', $fields['username']) -> get();
+            if($user -> count() <= 0) return;
+
+            $count = PostCount::where('username', $fields['username']) -> get();
+
+            if($count -> count() <= 0) {
+                PostCount::create([ 'username' => $fields['username'], 'count' => 1 ]);
+            }
+
+            else {
+                PostCount::where('username', $fields['username']) -> update(['count' => $count[0] -> count + 1 ]);
+                return response() -> json($count);
+            }
+            
+            return response() -> json($count);
+        }
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CommentsCountController;
+use App\Http\Controllers\PostsCountController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,8 +12,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/getCommentsCount', [CommentsCountController::class, 'getCount']);
+Route::get('/getPostCount', [PostsCountController::class, 'getCount']);
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login',[UserController::class, 'login']);
 Route::post('/logout',[UserController::class, 'logout']);
 Route::post('/incrementComments', [CommentsCountController::class, 'increment']);
+Route::post('/incrementPosts', [PostsCountController::class, 'increment']);

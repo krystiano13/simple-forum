5dda66c4197e31905a0704ae88e1cdf5136ab105
krystiano13 <crystianotv@gmail.com>
2023-09-22 10:14:31 +0200

    create models for comments_count and posts_count

---
+++ server/app/Models/CommentsCount.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentsCount extends Model
+{
+    use HasFactory;
+
+    /** 
+    * Table name
+    * @var string;
+    */
+
+    protected $table = 'posts_count';
+
+    /** 
+    * Primary key
+    * @var string;
+    */
+
+    protected $primaryKey = 'id';
+
+    /** 
+    * Fillable
+    * @var array;
+    */
+
+
+    protected $fillable = [
+        'id',
+        'username',
+        'count'
+    ];
+
+    /** 
+    * Hidden
+    * @var array;
+    */
+
+    protected $hidden = [
+        'updated_at',
+        'created_at'
+    ];
+}

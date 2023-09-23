d7aa70c4c50e65d30581e4b3eb0a4d962142b725
krystiano13 <crystianotv@gmail.com>
2023-09-23 13:05:31 +0200

    implement logging in on client side

--- client/src/routes/login/index.tsx
+++ client/src/routes/login/index.tsx
@@ -1,12 +1,37 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import { QwikSubmitEvent, component$, useStylesScoped$, useSignal, $ } from "@builder.io/qwik";
 import LoginStyles from "./login.css?inline";
 
 export default component$(() => {
   useStylesScoped$(LoginStyles);
 
+  const formRef = useSignal<HTMLFormElement>();
+
+  const logIn = $(async (e: QwikSubmitEvent<HTMLFormElement>, form: HTMLFormElement) => {
+    await fetch("http://127.0.0.1:8000/api/login", {
+      method: "POST",
+      body: new FormData(form),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === true) {
+          alert(data.message);
+          window.localStorage.setItem("user", data.token.name);
+          window.localStorage.setItem("token_id", data.token.id);
+          window.location.href = "/";
+        } else {
+          alert(data.message);
+        }
+      });
+  });
+
   return (
     <div class="formWrapper flex jc-center ai-center">
-      <form class="flex flex-col jc-center ai-center p-4 pt-8 pb-8 bg-primary">
+      <form
+        preventdefault:submit
+        ref={formRef}
+        onSubmit$={(e) => logIn(e, formRef.value as HTMLFormElement)}
+        class="flex flex-col jc-center ai-center p-4 pt-8 pb-8 bg-primary"
+      >
         <h1 class="m-3 color text-center f-h1 f-600 font-head">Sign in</h1>
         <input
           class="m-3 p-2 bg-secondary f-s color font-other"

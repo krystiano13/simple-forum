c53dd0c028ff859672a8745bcb07290615f0c427
krystiano13 <crystianotv@gmail.com>
2023-09-23 19:12:08 +0200

    implement logout on client side

--- client/src/routes/profile/index@navbar.tsx
+++ client/src/routes/profile/index@navbar.tsx
@@ -1,4 +1,10 @@
-import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  component$,
+  useSignal,
+  useStylesScoped$,
+  useVisibleTask$,
+  $,
+} from "@builder.io/qwik";
 import User from "../../../public/user.png?jsx";
 import ProfileStyles from "./profile.css?inline";
 
@@ -7,8 +13,35 @@ export default component$(() => {
   const since = useSignal<string>("00-00-00");
   const comments = useSignal<number>(0);
   const posts = useSignal<number>(0);
+  const formData = useSignal<FormData>(new FormData());
 
   useStylesScoped$(ProfileStyles);
+
+  const logout = $(async () => {
+    await fetch("http://127.0.0.1:8000/api/logout", {
+      method: "POST",
+      body: formData.value,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === true) {
+          localStorage.removeItem("user");
+          localStorage.removeItem("token_id");
+          window.location.href = "/";
+        }
+      });
+  });
+
+  useVisibleTask$(() => {
+    if (!localStorage.getItem("user") || !localStorage.getItem("token_id")) {
+      window.location.href = "/";
+    } else {
+      formData.value.append(
+        "token_id",
+        localStorage.getItem("token_id") as string
+      );
+    }
+  });
 
   return (
     <div class="width-100 height-100 profileWrapper flex jc-center ai-center">
@@ -18,7 +51,9 @@ export default component$(() => {
         <p class="m-2 font-other color f-m">Joined : {since.value}</p>
         <p class="m-2 font-other color f-m">Comments : {comments.value}</p>
         <p class="m-2 font-other color f-m">Posts : {posts.value}</p>
-        <button class="btn c-pointer m-2 bg-secondary color font-head p-1 pl-6 pr-6">Logout</button>
+        <button onClick$={logout} class="btn c-pointer m-2 bg-secondary color font-head p-1 pl-6 pr-6">
+          Logout
+        </button>
       </div>
     </div>
   );

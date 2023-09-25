ca562b96b3cfc0be4a6dfe745fb5e872e068bfd5
krystiano13 <crystianotv@gmail.com>
2023-09-25 20:27:05 +0200

    implement adding comments on backend

--- server/app/Http/Controllers/CommentController.php
+++ server/app/Http/Controllers/CommentController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -11,7 +13,33 @@ class CommentController extends Controller
         $fields = $request -> all();
 
         $validator = Validator::make($fields,[
-            
+            'username' => ['required'],
+            'post_id' => ['required'],
+            'content' => ['required', 'min:3']
         ]);
+
+        if($validator -> fails()) {
+            return response() -> json([
+                'status' => false,
+                'message' => "Validation Error",
+                "errors" => $validator -> errors()
+            ]);
+        }
+
+        $postQuery = Post::where('id', $fields['post_id']) -> get();
+
+        if($postQuery -> count() <= 0) {
+            return response() -> json([
+                'status' => false,
+                'message' => "Post doesn't exist"
+            ]);
+        }
+
+        Comment::create($fields);
+
+        return response() -> json([
+            'status' => true,
+            'message' => 'Comment created successfully'
+        ],200);
     }
 }

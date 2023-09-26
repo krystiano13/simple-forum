104c12d07effacbdd65c3416e842155544d453c0
krystiano13 <crystianotv@gmail.com>
2023-09-26 15:43:00 +0200

    create function that adds news to database

--- server/app/Http/Controllers/NewsController.php
+++ server/app/Http/Controllers/NewsController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class NewsController extends Controller
 {
-    //
+    public function createNews(Request $request) {
+        $fields = $request -> all();
+
+        $validator = Validator::make($fields,[
+            'title' => ['required'],
+            'content' => ['required', 'min:3'],
+            'username' => ['required']
+        ]);
+
+        if($validator -> fails()) {
+            return response() -> json([
+                'status' => false,
+                'message' => "Couldn't create news"
+            ]);
+        }
+
+        News::crete($fields);
+
+        return response() -> json([
+            'status' => true,
+            'message' => 'News added'
+        ],200);
+    }
 }

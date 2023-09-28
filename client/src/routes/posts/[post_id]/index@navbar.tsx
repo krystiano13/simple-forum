3989043908c8de1eb537d21615e2726ce38fb93a
krystiano13 <crystianotv@gmail.com>
2023-09-28 15:34:40 +0200

    create base structure for post component

--- client/src/routes/posts/[post_id]/index@navbar.tsx
+++ client/src/routes/posts/[post_id]/index@navbar.tsx
@@ -1,36 +1,65 @@
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
+import {
+  component$,
+  useVisibleTask$,
+  useStylesScoped$,
+} from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
+import BestStyles from "../../../components/best/Best.css?inline";
 
 interface dataInterface {
-    status: boolean,
-    post?: {
-        id: number,
-        created_at: string,
-        username: string,
-        title: string,
-        content: string
-    }
+  status: boolean;
+  post?: {
+    id: number;
+    created_at: string;
+    username: string;
+    title: string;
+    content: string;
+  };
 }
 
 export const usePost = routeLoader$(async (requestEvent) => {
-    const response = await fetch(`http://127.0.0.1:8000/api/getOnePost/${requestEvent.params.post_id}`);
-    const data = await response.json();
+  const response = await fetch(
+    `http://127.0.0.1:8000/api/getOnePost/${requestEvent.params.post_id}`
+  );
+  const data = await response.json();
 
-    return data as dataInterface;
+  return data as dataInterface;
 });
 
-export default component$(() => { 
-    const post = usePost();
+export default component$(() => {
+  const post = usePost();
 
-    useVisibleTask$(() => { 
-        if (post.value.status === false) {
-            window.location.href = '/';
-        }
-    });
+  useStylesScoped$(BestStyles);
 
-    return (
-        <div>
+  useVisibleTask$(() => {
+    if (post.value.status === false) {
+      window.location.href = "/";
+    }
+  });
 
+  return (
+    <div class="container container-xxl row jc-center ai-center">
+      <div class="block col-12">
+        <div class="best flex jc-center flex-col pl-6">
+          <h2 class="f-xl font-head f-600 color text-left mt-6">
+            {post.value.post?.title}
+          </h2>
+          <div class="posts mt-2 bg-primary p-3">
+            <div class="users mt-2">
+              <div class="user">
+                <div class="block bg-secondary color p-2 m-1 br-1 w-50">
+                  <h2 class="font-head f-600 f-m">
+                    {post.value.post?.username}
+                  </h2>
+                  <p class="font-other f-400 f-xs">
+                    created at: {post.value.post?.created_at}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 });

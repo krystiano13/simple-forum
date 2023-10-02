7fd46da354aa845bfe7cbd4f5ab5e0cf67e34d95
krystiano13 <crystianotv@gmail.com>
2023-10-02 15:39:50 +0200

    create styles for admin login panel

--- server/resources/views/adminForm.blade.php
+++ server/resources/views/adminForm.blade.php
@@ -1,9 +1,11 @@
 @extends('layout')
 
 @section('content')
-    <h1>Admin Panel</h1>
-    <form method="POST" action="/adminLogout">
-        @csrf
-        <button type="submit">Logout</button>
-    </form>
+    <main>
+        <h1>Admin Panel</h1>
+        <form method="POST" action="/adminLogout">
+            @csrf
+            <button type="submit">Logout</button>
+        </form>  
+    </main>
 @endsection
--- server/resources/views/adminLogin.blade.php
+++ server/resources/views/adminLogin.blade.php
@@ -1,11 +1,21 @@
 @extends('layout')
 
 @section('content')
-  <main>
-    <form method="POST" action="/attemptAdminLogin">
+  <main class="width-100 height-100 bg flex jc-center ai-center">
+    <form class="border-top-solid border-top-3 border-top-accent
+      bg-primary flex flex-col ai-center jc-center p-3 pt-12 pb-12" 
+      method="POST" action="/attemptAdminLogin">
       @csrf
-      <input type="password" name="password" />
-      <button type="submit">Log In</button>
+      <input class="m-1 p-1 font-other outline-none
+      bg-secondary border-none color border-bottom-solid
+      border-bottom-accent border-bottom-2 f-s" 
+      type="password" name="password" 
+      placeholder="password" />
+      <button class="m-1 mt-3 p-1 pl-4 pr-4 c-pointer font-head
+      bg-accent border-none color-text border-bottom-3
+      border-bottom-solid border-bottom-secondary
+      f-600 f-s" 
+      type="submit">Log In</button>
     </form>
   </main>
 @endsection
